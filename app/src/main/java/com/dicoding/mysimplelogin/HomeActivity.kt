2f79688ba73f54fcc28dd695b9d56ee19b6ebe8e
Sahir <sahirsyatha77@gmail.com>
2021-05-18 11:27:13 +0700

    implemented koin

---
+++ app/src/main/java/com/dicoding/mysimplelogin/HomeActivity.kt
@@ -0,0 +1,32 @@
+package com.dicoding.mysimplelogin
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.mysimplelogin.databinding.ActivityHomeBinding
+import org.koin.android.ext.android.inject
+
+class HomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
+    val userRepository: UserRepository by inject()
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvWelcome.text = "Welcome ${userRepository.getUser()}"
+
+        binding.btnLogout.setOnClickListener {
+            userRepository.logoutUser()
+            moveToMainActivity()
+        }
+    }
+
+    private fun moveToMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
